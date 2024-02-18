dc82c5cc22cae0abbfaefdc4d9ed0c1aa40e4d03
dodslaser <2969909+dodslaser@users.noreply.github.com>
2024-02-18 16:16:18 +0100

    chore: Restructure more

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,6 +1,15 @@
+"""Test tests."""
+
+import json
+import functools
+
 def test_pass():
+    """A test that should pass."""
     assert True
 
+
 def test_fail():
+    """A test that should fail."""
     assert False
-    
+
+# THIS LINE IS TOO LONG FOR BLACK! THIS LINE IS TOO LONG FOR BLACK! THIS LINE IS TOO LONG FOR BLACK! THIS LINE IS TOO LONG FOR BLACK! THIS LINE IS TOO LONG FOR BLACK! THIS LINE IS TOO LONG FOR BLACK!
